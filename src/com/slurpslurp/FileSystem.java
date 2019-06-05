cfdb66c686262348b750e069a8ec5ac5c0488cee
Alexandre <alex.sallinen@gmail.com>
2019-06-05 21:55:41 +0200

    Filesystem

---
+++ src/com/slurpslurp/FileSystem.java
@@ -0,0 +1,23 @@
+package com.slurpslurp;
+
+import java.io.File;
+
+public class FileSystem {
+    void moveToFolder(String string){
+
+        File folder = new File(string);
+        if (!folder.exists())
+            folder.mkdir();
+
+        String home = System.getProperty("user.home");
+        File file = new File(home+"/Downloads" );
+
+        File[] content = file.listFiles();
+
+        for(int i = 0; i < content.length; i++) {
+            File newF = new File(string + content[i].getName());
+            content[i].renameTo(newF);
+        }
+    }
+
+}
